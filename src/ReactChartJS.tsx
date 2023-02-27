fa46fcfd4e4e6a392814be007ee4b23135c438d6
Vincent Van Herreweghe <Vincent.VanHerreweghe@inetum-realdolmen.world>
2023-02-27 21:21:18 +0100

    add Daikin requirements to ChartJS

--- src/ReactChartJS.tsx
+++ src/ReactChartJS.tsx
@@ -1,151 +1,225 @@
-import {Navigation} from "./Navigation";
+import React from "react";
 import {
     Chart as ChartJS,
-    CategoryScale,
     LinearScale,
+    CategoryScale,
+    BarElement,
     PointElement,
     LineElement,
-    Title,
-    Tooltip,
     Legend,
+    Tooltip,
+    LineController,
+    BarController, Title, ChartOptions, ChartData,
 } from 'chart.js';
-import {Line} from 'react-chartjs-2';
-import {faker} from "@faker-js/faker";
-import {saveAs} from "file-saver";
-import {useRef} from "react";
+import { Chart } from 'react-chartjs-2';
+import faker from 'faker';
+import {Navigation} from "./Navigation";
+import Zoom from "chartjs-plugin-zoom";
+import annotationPlugin from 'chartjs-plugin-annotation';
+
 ChartJS.register(
-    CategoryScale,
     LinearScale,
+    CategoryScale,
+    BarElement,
     PointElement,
     LineElement,
-    Title,
+    Legend,
     Tooltip,
-    Legend
+    LineController,
+    BarController,
+    Title,
+    Zoom,
+    annotationPlugin
 );
-import 'chartjs-plugin-zoom';
 
+const arr = [];
+for (let i=0; i < 24; i++) {
+    for (let j=0; j < 2; j++) {
+        arr.push(`${i}:${j === 0 ? `00` : 30*j}`);
+    }
+}
 
-const options = {
-    responsive: true,
+const getHouse = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = 230;
+    canvas.height = 210;
+    const ctx = canvas.getContext('2d');
+    if(ctx) {
+        ctx.fillStyle = '#666';
+        ctx.strokeStyle = '#666';
+        ctx.lineWidth = 10;
+        ctx.strokeRect(40, 90, 150, 110);
+        ctx.fillRect(95, 140, 40, 60);
+        ctx.beginPath();
+        ctx.moveTo(15, 90);
+        ctx.lineTo(115, 10);
+        ctx.lineTo(215, 90);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    return canvas;
+}
+
+const labels = [...arr];
+
+const options: ChartOptions = {
     plugins: {
-        legend: {
-            position: 'top' as const,
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart',
-        },
         zoom: {
+            limits: {
+                x: {min: 0, max: 50, minRange: 50},
+                y: {min: -200, max: 2000, minRange: 50}
+            },
             zoom: {
                 wheel: {
                     enabled: true,
                 },
                 pinch: {
-                    enabled: true
+                    enabled: true,
                 },
+                mode: 'xy'
             },
             pan: {
                 enabled: true,
+                mode: 'xy'
             }
         },
-        lineMarker: {
-            id: 'lineMarker',
-            beforeDatasetsDraw(chart, args, options): boolean | void {
-                const {ctx, chartArea: {top, bottom}, scales: {x}} = chart;
-                ctx.save();
-                
-                ctx.beginPath();
-                ctx.strokeStyle = "blue";
-                ctx.lineWidth = 5;
-                ctx.moveTo(x.getPixelForValue(2), top);
-                ctx.lineTo(x.getPixelForValue(2), bottom);
-                ctx.stroke();
+        annotation: {
+            annotations: {
+                box1: {
+                    type: 'box',
+                    xMin: 5,
+                    xMax: 30,
+                    yMin: -200,
+                    yMax: 2000,
+                    backgroundColor: 'rgba(255, 99, 132, 0.25)',
+                    borderColor: "white"
+                },
+                line: {
+                    type: 'line',
+                    scaleID: 'x',
+                    value: 20,
+                    borderColor: 'rgb(255, 99, 132)',
+                    borderWidth: 3,
+                    label: {
+                        display: true,
+                        position: 'start',
+                        backgroundColor: 'rgba(245,245,245, 255)',
+                        content: getHouse(),
+                        width: '10%',
+                        height: '10%',
+                        borderWidth: 0,
+                        font: {
+                            size: 10
+                        },
+                    }
+                },
             }
+        },
+        title: {
+            display: true,
+            text: "Chart JS",
+            color: 'black'
         }
     },
     scales: {
-        // multi axis possible (https://www.chartjs.org/docs/latest/samples/line/multi-axis.html)
         y: {
-            type: 'linear',
+            type: 'linear' as const,
             display: true,
             position: 'left',
+            min: -40,
+            max: 60,
+            ticks: {
+                stepSize: 5
+            },
+            grid: {
+                display: false
+            }
         },
         y1: {
-            type: 'linear',
+            type: 'linear' as const,
             display: true,
-            position: 'left',
-            grid: {
-                drawOnChartArea: false, // only want the grid lines for one axis to show up
+            position: "left",
+            min: -300,
+            max: 1200,
+            ticks: {
+                stepSize: 50
             },
+            grid: {
+                display: false
+            }
+        },
+        y2: {
+            type: 'linear' as const,
+            display: true,
+            position: "left",
+            min: -25,
+            max: 325,
+            ticks: {
+                stepSize: 25
+            },
+            grid: {
+                display: false
+            }
+        },
+        y3: {
+            type: 'linear' as const,
+            display: true,
+            position: "left",
+            min: -5,
+            max: 25,
+            ticks: {
+                stepSize: 1
+            }
         }
-        
-    },
-};
+    }
+}
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-let data = {
+export const data: ChartData = {
     labels,
     datasets: [
         {
+            type: 'line' as const,
             label: 'Dataset 1',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
             borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            yAxisId: "y1"
+            backgroundColor: 'rgb(255, 99, 132)',
+            fill: false,
+            data: labels.map(() => faker.datatype.number({ min: -30, max: 50 })),
+            yAxisID: 'y'
         },
         {
+            type: 'bar' as const,
             label: 'Dataset 2',
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgb(75, 192, 192)',
+            data: labels.map(() => faker.datatype.number({ min: -200, max: 1000 })),
+            yAxisID: 'y1'
+        },
+        {
+            type: 'bar' as const,
+            label: 'Dataset 3',
             borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            yAxisId: "y"
+            backgroundColor: 'rgb(53, 162, 235)',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 300 })),
+            yAxisID: 'y2'
+        },
+        {
+            type: 'line' as const,
+            label: 'Dataset 4',
+            borderColor: 'rgb(31, 64, 82)',
+            backgroundColor: 'rgb(31, 64, 82)',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 20 })),
+            yAxisID: 'y3'
         },
     ],
 };
 
-/**
- * @param b64Data
- * @param contentType
- * @param sliceSize
- * @returns {Blob}
- * @link https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
- */
-const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-        const byteNumbers = new Array(slice.length);
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < slice.length; i += 1) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-    }
-
-    return new Blob(byteArrays, { type: contentType });
-};
-
 export const ReactChartJS = () => {
-    const chartRef = useRef(null);
-    return (
-        <>
-            <Navigation />
-            <div style={{width: "100%", height: "600px", display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "20px", marginTop: "20px"}}>
-                {/* add chartRef to later download it as png */}
-                <Line options={options} data={data} ref={chartRef}/>
-            </div>
-            <button onClick={() => {
-                const b64 = chartRef.current!.toBase64Image().replace('data:image/png;base64,', '');
-                const content = b64toBlob(b64);
-                const file = new File([content], 'Revenue_chart.png', { type: 'image/png' });
-                saveAs(file);
-            }}>download chartjs chart</button>
-        </>
-    );
-};
+    return <>
+        <Navigation />
+        <div style={{width: 1000, margin: "5% auto"}}>
+            <Chart type={'bar'} data={data} options={options}  />
+        </div>
+            
+        </>;
+}
